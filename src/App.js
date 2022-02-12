53bc6989c8cb04bb4916d989f256b85484507f81
Cold Code <dee_femifose@yahoo.com>
2022-02-12 00:55:21 -0600

    Working on Menu and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; //Link 
+import { Layout } from 'antd'; //Typography Space
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css'
+
+
+const App = () => {
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar/>
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        {/* <Routes> */}
+                            {/* <Route exact path='/'>
+                                <Homepage/>
+                            </Route>
+                            <Route exact path='/exchanges'>
+                                <Exchanges/>
+                            </Route>
+                            <Route exact path='/cryptocurrencies'>
+                                <Cryptocurrencies/>
+                            </Route>
+                            <Route exact path='/crypto/:coinId'>
+                                <CryptoDetails/>
+                            </Route>
+                            <Route exact path='/news'>
+                                <News/>
+                            </Route> */}
+                        {/* </Routes> */}
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'>
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
